bd8a72660b8e708cf9d326c40b9db30cd1a91e45
Алавутдин Гебеков < >
2017-03-11 20:07:11 +0300

    Добавил немного играбельности

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,16 @@
 var scope=0;
+var lives=3;
+var liveTime=3;
+var star;
+var isOver=false;
 
 var game=new Phaser.Game (800,600, Phaser.AUTO, '', {preload: preload, create:create, update:update});
-var player; var platforms; var cursors; var stars; var _scope;
 function preload() {
-    game.load.image('sky','img/background.png');
+    game.load.image('background','img/background.png');
     game.load.image('ground','img/platform.png');
     game.load.image('star','img/star.png');
-    //Загрузка спрайт листа
     game.load.spritesheet('dude','img/dude.png',32,48);
+    game.load.spritesheet('boom','img/explosion.png',64,64,23);
 }
 
 function create() {
@@ -15,7 +18,13 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);    
 
     //Добавляем спрайты
-    game.add.sprite(0,0,'sky');
+    game.add.sprite(0,0,'background');
+
+    bombs=game.add.group();
+    var bomb=bombs.create(0,0,'boom',[0], false);
+    bomb.anchor.setTo(0.5, 0.5);
+    bomb.animations.add('boom');
+
     createStars();
     //Создадим группу объектов платформ, на которые можно будет запрыгивать
     platforms=game.add.group();
@@ -56,7 +65,7 @@ function create() {
     //Используем две анимации, т.е. смена фреймов со скоростью 10 ф/с
     //последний параметр true указывает на цикличность
     player.animations.add('left',[0,1,2,3],10,true);
-    player.animations.add('right',[5,6,7,8],10,true);
+    player.animations.add('right',[5,6,7,8],10,true);    
 
     //Настройка управления персонажем
     cursors=game.input.keyboard.createCursorKeys();
@@ -65,35 +74,43 @@ function create() {
 
     _scope = game.add.text(10, 10, "Счет: 0", {
         font: "14px Arial",
-        fill: "#000",
-        align: "center"
+        fill: "#000"
     });
+
+
+    game.time.events.loop(Phaser.Timer.SECOND, starLiveTimer, this);
 
 }
 
 function update () {
-    //Добавим проверку на столкновения
-    var hitPlatform = game.physics.arcade.collide(player,platforms);
-    game.physics.arcade.collide(stars, platforms);
-    game.physics.arcade.collide(player,stars,collectStar,null,this);
-    _scope.setText("Счет: "+scope);
-    player.body.velocity.x=player.body.velocity.x/1.05;
-    if (cursors.left.isDown) {
-        player.body.velocity.x=-450;
-        player.animations.play('left');
-    } else if (cursors.right.isDown) {
-        player.body.velocity.x=450;
-        player.animations.play('right');
-    } else {
-        player.animations.stop();
-        player.frame=4;
-    }
+    if (!isOver) {
+        //Добавим проверку на столкновения
+        var hitPlatform = game.physics.arcade.collide(player,platforms);
+        game.physics.arcade.collide(star, platforms);
+        game.physics.arcade.collide(player,star,collectStar,null,this);
+        
+        //Управление персонажем
+        player.body.velocity.x=player.body.velocity.x/1.05;
+        if (cursors.left.isDown) {
+            player.body.velocity.x=-450;
+            player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            player.body.velocity.x=450;
+            player.animations.play('right');
+        } else {
+            player.animations.stop();
+            player.frame=4;
+        }
 
-    //Игрок может отпрыгивать от платформы
-    if (cursors.up.isDown&&player.body.touching.down&&hitPlatform) {
-        player.body.velocity.y=-600;
-    }
+        //Игрок может отпрыгивать от платформы
+        if (cursors.up.isDown&&player.body.touching.down&&hitPlatform) {
+            player.body.velocity.y=-600;
+        }
 
+        /* Дополнительные операции */
+
+        _scope.setText("Счет: "+scope+"\nЖизней: "+lives);
+    }
 }
 
 function collectStar(player,star) {
@@ -103,9 +120,32 @@ function collectStar(player,star) {
 }
 
 function createStars() {
-    stars=game.add.group();
-    stars.enableBody=true;
-    var star = stars.create(Math.random()*(game.world.width-32)+32,0,'star');
+    liveTime=4;
+    star = game.add.sprite(Math.random()*(game.world.width),0,'star');
+    game.physics.arcade.enable(star);
     star.body.gravity.y = 800;
-    star.body.bounce.y=0.3+Math.random()*0.2;    
+    star.body.bounce.y=0.3+Math.random()*0.2;  
+}
+
+function starLiveTimer() {
+    liveTime--;
+    if (liveTime<0) starDeath();    
+}
+
+function starDeath() {
+    star.kill();
+    lives--;
+    var bomb=bombs.getFirstExists(false);
+    bomb.reset(star.x,star.y);
+    bomb.play('boom',30,false,true);
+    createStars();
+    if (lives<0) {
+        isOver=true;
+
+        _scope = game.add.text(430,320, "КОНЕЦ", {
+        font: "70px Arial",
+        fill: "#000"
+    });
+
+    }
 }
